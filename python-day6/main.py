83e4a1b1c201c7ec87c0ecc1b56946906c9626ec
Handika <handika00026@gmail.com>
2024-03-12 12:54:11 +0700

    python day 6

---
+++ python-day6/main.py
@@ -0,0 +1,21 @@
+# Function
+def my_function():
+    print('Ini function buatan kita sendiri')
+  
+# memanggil function
+my_function()
+
+# While loop
+"""
+while something_is_true:
+    # do this
+"""
+
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+    
+"""
+Dalam contoh di atas, loop while akan terus berjalan selama count kurang dari 5. Setiap iterasi, nilai count dinaikkan satu.
+"""

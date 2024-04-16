e19eb2d174d1968838646049162d7ed8d3c7a937
Handika <handika00026@gmail.com>
2024-04-16 21:22:31 +0700

    OOP

---
+++ python-day16/main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+loreng = Turtle()
+print(loreng)
+loreng.shape("turtle")
+loreng.color("magenta")
+loreng.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()

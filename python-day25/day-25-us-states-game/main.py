1f8eb9e233cc48a4908522ddbc339c5b23f77228
Handika <handika00026@gmail.com>
2024-06-22 00:07:22 +0700

    day 25 us states game

--- python-day25/day-25-us-states-game/main.py
+++ python-day25/day-25-us-states-game/main.py
@@ -11,6 +11,7 @@ turtle.mainloop()
 """
 
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S States Game")
@@ -18,5 +19,35 @@ image = "./blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+data = pandas.read_csv('50_states.csv')
+all_states = data.state.to_list()
+guessed_states = []
 
-screen.mainloop()
+while len(guessed_states) < len(all_states):
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/{len(all_states)} States Correct",
+                                    prompt="Whats's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+
+        new_data = {
+            "missing state": missing_states
+        }
+
+        df = pandas.DataFrame(new_data)
+        df.to_csv("state_to_learn.csv")
+
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.penup()
+        t.hideturtle()
+        state_data = data[data.state == answer_state]
+        x_cor = state_data.x.item()
+        y_cor = state_data.y.item()
+        t.teleport(x_cor, y_cor)
+        t.write(state_data.state.item())
